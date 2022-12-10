dc84b5f6c6c916f6f07add617a85abf945568d2a
GuilhermeFMP <guilhermemoraispinho@gmail.com>
2022-12-10 11:04:20 -0300

    Requisito 7 ok e mudei a table de componente

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
+    const { expenses } = this.props;
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </thead>
+        <tbody>
+          { expenses.map((expense, index) => {
+            const { description, value, tag, method, exchangeRates, currency } = expense;
+            const money = Object.values(exchangeRates !== undefined
+              && exchangeRates).find((exchange) => exchange.code === currency);
+            const { name, ask } = money;
+            return (
+              <tr key={ index }>
+                <td>{description}</td>
+                <td>{tag}</td>
+                <td>{method}</td>
+                <td>{Number(value).toFixed(2)}</td>
+                <td>{name}</td>
+                <td>{Number(ask).toFixed(2)}</td>
+                <td>{(Number(ask) * Number(value)).toFixed(2)}</td>
+                <td>Real</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+Table.propTypes = {
+  expenses: PropTypes.instanceOf(Array).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 import { fetchAPI } from '../redux/actions';
 
 class Wallet extends React.Component {
@@ -16,17 +17,7 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <WalletForm />
-        <table>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </table>
+        <Table />
       </div>
     );
   }
