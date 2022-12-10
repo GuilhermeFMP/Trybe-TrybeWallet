6ec450415a6ef9d8649bb3e60b761df639a95acd
GuilhermeFMP <guilhermemoraispinho@gmail.com>
2022-12-10 09:29:18 -0300

    Requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -16,6 +16,17 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <WalletForm />
+        <table>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </table>
       </div>
     );
   }
